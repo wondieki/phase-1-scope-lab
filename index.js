fa2270aa14eceb794c9e7c2c9f875c71a6c92655
wondieki <wkerubo28@gmail.com>
2024-10-07 14:49:33 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+ customerName=customerName.toUpperCase();
+}
+var bestCustomer
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "i think its bob";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bob";
+  return leastFavoriteCustomer;
+}
